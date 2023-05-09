684a7c49339dfb74e6758edf44b66f32cd3ae180
Stephen Oduh <stevihiene@gmail.com>
2023-05-08 23:08:57 -0700

    4th commit

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 number = 98
-print(f"{number} battery street\n")
+print("{:d} Battery street".format(number))
--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 number = 3.14159
-print("float:", f'{number:.2f}\n')
+print("float:", f'{number:.2f}')
+print(" ")
