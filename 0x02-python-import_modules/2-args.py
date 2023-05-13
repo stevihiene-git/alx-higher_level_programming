5d4532efcca6d107a7736e99bc917563a5ea7708
Stephen Oduh <stevihiene@gmail.com>
2023-05-13 05:59:33 -0700

    co

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    """
-    Prints the argument list passed to the program
+import sys
+
+if __name__ == '__main__':
+    """Prints the argument list passed to the program
 
     The program takes all the arguments starting from the second
     and prints the number of arguments and their value
 
     """
+    sys_arg = sys.argv
+    arg_lenght = len(sys_arg) - 1
 
-    import sys
-    arg_count = len(sys.argv)-1
-
-    if arg_count == 0:
-        print("0 argument")
-    elif arg_count == 1:
-        print("1 argument")
-    else:
-        for x in range(arg_count):
-            print("{} : {}".format(x+1, sys.argv[x+1]))
+    if arg_lenght > 1:
+        print(arg_lenght, 'arguments:')
+        for i in range(1, arg_lenght + 1):
+            print('{:d}: {}'.format(i, sys_arg[i]))
+    elif arg_lenght == 1:
+        print(arg_lenght, 'argument:')
+        for i in range(1, arg_lenght + 1):
+            print('{:d}: {}'.format(i, sys_arg[i]))
+    elif l_av == 0:
+        print(arg_lenght, 'arguments.')
