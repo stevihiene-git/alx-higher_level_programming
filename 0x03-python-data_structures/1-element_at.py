9842458d78b318a6c3670306c103918705626f19
Stephen Oduh <stevihiene@gmail.com>
2023-05-15 08:16:08 -0700

    2nd commit

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    count = len(my_list)
+    if idx < 0 or idx > count - 1:
+        return none
+    else:
+        return my_list(idx)
---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    
+    list_lenght = len(my_list)
+
+    if my_list:
+        my_list.reverse()
+        
+        for i in range(len(list_lenght)
+        print("{:d}".format(my_list[i]))
