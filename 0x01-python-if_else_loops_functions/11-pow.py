9cee6d7a218138a04eaa1dfd2002d7bbd8541c48
Stephen Oduh <stevihiene@gmail.com>
2023-05-23 14:04:13 -0700

    python if-else

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def (a,b):
+    """return a^b"""
+    return (a**b)
--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for j in range(97,123): 
-        print(chr(j),end=" ")	
+"""Print the alphabet in lowercase, not followed by a new line."""
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")	
